66193c4c9c1b69cb0df6c75d66333376de8b7ef8
marianabarreiros <maridepp1@hotmail.com>
2017-05-17 12:16:23 -0300

    primeira WebApi

---
+++ AplicacaoWebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AplicacaoWebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AplicacaoWebApi/Controllers/WebController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AplicacaoDataAccess;
+
+namespace AplicacaoWebApi.Controllers
+{
+    public class WebController : ApiController
+    {
+        public IEnumerable<Aplicacoes> Get()
+        {
+            using (AplicationDBEntities entities = new AplicationDBEntities())
+            {
+                return entities.Aplicacoes.ToList();
+            }
+        }
+
+        public IEnumerable<Aplicacoes> Get(String id)
+        {
+            using (AplicationDBEntities entities = new AplicationDBEntities())
+            {
+                return entities.Aplicacoes.Where(e => e.Tipo == id).ToList();
+                //var entity = entities.Aplicacoes.Where(e => e.Tipo == id).ToList();
+                //if(entity != null)
+                //{
+
+                //    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                //}
+                //else
+                //{
+                //    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Aplicação com tipo " + id.ToString() + " não encontrada");
+                //}
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Aplicacoes aplicacoes)
+        {
+            try
+            {
+                using (AplicationDBEntities entities = new AplicationDBEntities())
+                {
+                    entities.Aplicacoes.Add(aplicacoes);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, aplicacoes);
+                    message.Headers.Location = new Uri(Request.RequestUri + aplicacoes.Tipo.ToString());
+
+                    return message;
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    }
+}
